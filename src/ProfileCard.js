8b5295a28e1132ae68f4f08bcc86ce97d30417c9
Tristan Cole <93275422+TCole79@users.noreply.github.com>
2022-11-21 16:18:25 -0700

    Including images
    * Add three logos
    * Add image prop into ProfileCard component
    * Add import statements and image props to App component

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,13 +1,8 @@
-function ProfileCard({ title, handle }) {
-    //below is same as above, but we can condense further as below
-    //const title = props.title
-    //const handle = props.handle
-
-    //below is same as above, but we can condense further as what is being called in ProfileCard
-    //const { title, handle } = props;
+function ProfileCard({ title, handle, image }) {
 
     return (
     <div>
+        <img src={ image } alt="pda logo" />
         <div>Title is { title }</div>
         <div> Handle is { handle }</div>
     </div>
